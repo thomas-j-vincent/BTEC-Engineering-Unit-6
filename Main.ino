4be14149cc385031fc84a855340eda201eeb3a63
thomas-j-vincent <thomasjvincent@outlook.com>
2026-01-13 14:23:33 +0000

    code logic completed, working on removing errors

--- Main.ino
+++ Main.ino
@@ -1,7 +1,7 @@
 #include <LiquidCrystal_I2C.h> // needs initialising in the arduino app
 LiquidCrystal_I2C lcd(0x27, 16, 20);
 
-    const int dcMotorPin = 2;
+    const int dcMotorPin = 5;
     bool motorValue = false;
     int onDuration;
 
@@ -11,8 +11,8 @@ LiquidCrystal_I2C lcd(0x27, 16, 20);
     const int lightSensor = A2;
     int lightValue;
 
-    const int emergencyStop = 5;
-    int  emergencyStopValue;
+    const int emergencyStop = 2;
+    volatile bool emergencyStopValue = false;
     const int continueButton = 6;
     int continueValue;
     const int smallButton = 7;
@@ -27,6 +27,7 @@ void setup()
     pinMode(greenLED, OUTPUT);
 
     pinMode(emergencyStop, INPUT);
+    attachInterrupt(digitalPinToInterrupt(emergencyStop), stopNow, FALLING);
     pinMode(continueButton, INPUT);
     pinMode(smallButton, INPUT);
     pinMode(mediumButton, INPUT);
@@ -41,28 +42,99 @@ void setup()
 
 void loop()
 {
-    lightValue = analodRead(lightSensor);
+    lightValue = analogRead(lightSensor);
 
-    emergencyStopValue = analogRead(emergencyStop);
-    continueValue = analogRead(continueButton);
-    smallValue = analogRead(smallValue);
-    mediumValue = analogRead(mediumValue);
-    if(continueValue == 1) {
+    //emergencyStopValue = analogRead(emergencyStop);
+    continueValue = digitalRead(continueButton);
+    smallValue = digitalRead(smallValue);
+    mediumValue = digitalRead(mediumValue);
+
+    if(mediumValue == 1) {
+        lcd.clear();
         while(lightValue > 800) { // adjust number for lightness (higher is darker)
-                lcd.clear();
-                lcd.setCursor(0,0); 
-                lcd.print("place suitcase");
-                lcd.setCursor(0,1); 
-                lcd.print("on machine");
+            lcd.setCursor(0,0); 
+            lcd.print("place suitcase");
+            lcd.setCursor(0,1); 
+            lcd.print("on machine");
+        }
+        lcd.setCursor(0,0); 
+        lcd.print("option selected:");
+        lcd.setCursor(0,1);
+        lcd.print("medium"); 
+
+        digitalWrite(dcMotorPin, HIGH);
+        motorValue = true;
+        onDuration =  (10000 + millis()); 
+        if (millis() == onDuration) { 
+            digitalWrite(dcMotorPin, LOW);
+            motorValue =  false;
+        }
+    }
+
+    if(smallValue == 1) {
+        lcd.clear();
+        while(lightValue > 800) { // adjust number for lightness (higher is darker)
+            lcd.setCursor(0,0); 
+            lcd.print("place suitcase");
+            lcd.setCursor(0,1); 
+            lcd.print("on machine");
+        }
+        lcd.setCursor(0,0); 
+        lcd.print("option selected:");
+        lcd.setCursor(0,1);
+        lcd.print("small"); 
+
+        digitalWrite(dcMotorPin, HIGH);
+        motorValue = true;
+        onDuration =  (5000 + millis()); 
+        if (millis() == onDuration) { 
+            digitalWrite(dcMotorPin, LOW);
+            motorValue =  false;
+        }
+    }
+
+    if(continueValue == 1) {
+        lcd.clear();
+        while(lightValue > 800) { // adjust number for lightness (higher is darker)
+            lcd.setCursor(0,0); 
+            lcd.print("place suitcase");
+            lcd.setCursor(0,1); 
+            lcd.print("on machine");
         } 
-        lcd.clear()
         lcd.setCursor(0,0); 
         lcd.print("option selected:");
         lcd.setCursor(0,1);
         lcd.print("continue");
 
         digitalWrite(dcMotorPin, HIGH);
-        motorValue == true;
+        motorValue = true;
+        onDuration =  (5000 + millis()); 
+        if (millis() == onDuration) { 
+            digitalWrite(dcMotorPin, LOW);
+            motorValue =  false;
+        }
     }
+
+    if(motorValue == true) {
+        digitalWrite(redLED, HIGH);
+        digitalWrite(greenLED, LOW);
+    } else if(motorValue == false) {
+        digitalWrite(redLED, LOW);
+        digitalWrite(greenLED, HIGH);
+    }
+
+    if (emergencyStopValue) {
+        lcd.clear();
+        lcd.setCursor(0,0); 
+        lcd.print("option selected:");
+        while (true) {
+            digitalWrite(dcMotorPin, LOW);
+            motorValue =  false;
+        // Infinite loop = program effectively stopped
+        }
+    }
+}
+void stopNow() {
+    emergencyStopValue = true;
 }
 
